5a6b7ae2e30e1d87d710245a7d1ef288699de815
pritam hinger <pritamgenius24@gmail.com>
2017-09-25 21:56:47 +0530

    Initial Commit

---
+++ OnBoarding/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnBoarding
+//
+//  Created by Pritam Hinger on 25/09/17.
+//  Copyright © 2017 AppDevelapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
