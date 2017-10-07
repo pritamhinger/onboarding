a948e76131c85907c45ffec407c4911fa1e79014
pritam hinger <pritamgenius24@gmail.com>
2017-10-07 15:20:26 +0530

    Handle Screen Rotation Scenarios.

--- OnBoarding/Controller/ViewController.swift
+++ OnBoarding/Controller/ViewController.swift
@@ -175,6 +175,16 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             self.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
         }, completion: nil)
     }
+    
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        collectionView.collectionViewLayout.invalidateLayout()
+        
+        let indexPath = IndexPath(item: pageControl.currentPage, section: 0)
+        DispatchQueue.main.async {
+            self.collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+            self.collectionView.reloadData()
+        }
+    }
 }
 
 // MARK: - UICollectionViewDataSource Implementation
